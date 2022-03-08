e2b943b03ab291d7fdfa33b8c2eb728fe1d1a4d8
k3n0nlyy <kenolychanei@gmail.com>
2022-03-08 23:25:15 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="responsive.css">
+<title>Title</title>
+</head>
+<body>
+
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+
+<section class="container-fluid aboutbg text-center">
+    <div class="container">
+        <h2>GAMES &amp; LEVEL DESIGNER</h2>
+        <p class="particle">.... and particle wizard</p>
+
+        <p class="abouttxt">My  name is josh Whitkin. I am a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects.  What makes me passionate about games is the combination of technology  and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
+          I always tell a story.</p>
+  
+      <div class="blocker"></div>
+
+    </div> <!-- container -->
+</section> <!-- container-fluid -->
+
+<section class="container-fluid articlesbg">
+    <div class="container">
+        <h3 class="text-center">LATEST ARTICLE</h3>
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port1.jpg"> alt="woman">
+                <h4 class="article-title">A PORTRAIT</h4>
+                <p class="date">Friday, November 10 2030</p>
+                <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
+                <a class="readmore" href="#">CONTINUE READING</a>
+            </div>
+            <div class="col-md-4">
+              <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg"> alt="woman">
+              <h4 class="article-title">A PORTRAIT</h4>
+              <p class="date">Friday, November 10 2030</p>
+              <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
+              <a class="readmore" href="#">CONTINUE READING</a>
+          </div>
+          <div class="col-md-4">
+              <img class="img-fluid" src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg"> alt="woman">
+              <h4 class="article-title">A PORTRAIT</h4>
+              <p class="date">Friday, November 10 2030</p>
+              <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
+              <a class="readmore" href="#">CONTINUE READING</a>
+          </div>
+
+        </div> <!-- row -->
+
+    </div> <!-- container -->
+</section> <!-- container-fluid -->
+
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
