6cbaa108e8c11c129a7ae540f58c995cf31b5740
k3n0nlyy <62008189+k3n0nlyy@users.noreply.github.com>
2022-04-05 15:53:08 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -18,31 +18,30 @@
 
 <section class="container-fluid articlesbg">
     <div class="container">
-        <h3 class="text-center">LATEST ARTICLE</h3>
+        <h3 class="text-center"><?php the_field('titleforarticles'); ?></h3>
         <div class="row">
             <div class="col-md-4">
-                <img class="img-fluid" src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/port1.jpg" alt="woman">
-                <h4 class="article-title">A PORTRAIT</h4>
-                <p class="date">Friday, November 10 2030</p>
-                <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
-                <a class="readmore" href="#">CONTINUE READING</a>
+                <img class="img-fluid" src="<?php the_field('articleimage');?>">
+                <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
+                <p class="date" ><?php the_field('articledate'); ?></p>
+                <p class="article-p"><?php the_field('articleexcerpt'); ?></p>
+                <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
             </div>
             <div class="col-md-4">
-              <img class="img-fluid" src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/port2.jpg" alt="woman">
-              <h4 class="article-title">A PORTRAIT</h4>
-              <p class="date">Friday, November 10 2030</p>
-              <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
-              <a class="readmore" href="#">CONTINUE READING</a>
-          </div>
-          <div class="col-md-4">
-              <img class="img-fluid" src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/port3.jpg" alt="woman">
-              <h4 class="article-title">A PORTRAIT</h4>
-              <p class="date">Friday, November 10 2030</p>
-              <p class="article-p">STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>
-              <a class="readmore" href="#">CONTINUE READING</a>
-          </div>
-
-        </div> <!-- row -->
+                <img class="img-fluid" src="<?php the_field('articleimage_middle');?>">
+                <h4 class="article-title"><?php the_field('articletitle_middle'); ?></h4>
+                <p class="date" ><?php the_field('articledate_middle'); ?></p>
+                <p class="article-p"><?php the_field('articleexcerpt_middle'); ?> </p>
+                <a href="<?php the_field('readmore_middle'); ?>" class="readmore">CONTINUE READING</a>
+            </div>
+            <div class="col-md-4">
+                <img class="img-fluid" src="<?php the_field('articleimage_end');?>">
+                <h4 class="article-title"><?php the_field('articletitle_end'); ?></h4>
+                <p class="date" ><?php the_field('articledate_ending'); ?></p>
+                <p class="article-p"><?php the_field('articleexcerpt_end'); ?> </p>
+                <a href="<?php the_field('readmore_end'); ?>" class="readmore">CONTINUE READING</a>
+            </div>
+          </div> <!-- row -->
 
     </div> <!-- container -->
 </section> <!-- container-fluid -->
