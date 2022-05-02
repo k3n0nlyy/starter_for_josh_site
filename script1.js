2c1e8132c9e6a57c8623191fa73cf32eb20e1e29
k3n0nlyy <62008189+k3n0nlyy@users.noreply.github.com>
2022-05-02 13:22:14 +0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+    scrollTrigger: {
+     trigger: '.aboutbg',
+     markers:false,
+     start:"bottom 90%", 
+     end:"top 30%",
+     scrub:3, 
+          }
+  
+});
+
+tl.from('.abouttxt',{
+ duration:1,
+      y:250,
+ opacity:0,
+}).from('.blocker',{
+ duration:1,
+ x:250,
+opacity:0,
+});
+
+gsap.timeline()
+.from('.navbar', {scale:0, duration:2})
+.from('.navbar-brand', {opacity:0, scale: 0, ease:"back"})
+.from('.tag', {opacity:0, y: 10, ease:"back"});
+
+/* gsap.from('.navbar-brand', {opacity:0, duration:2, y:-50, ease:"bounce.out"});
+gsap.from('.tag', {opacity:0, duration:2, x:50, ease:'bounce.out', stagger:0.6}); */
